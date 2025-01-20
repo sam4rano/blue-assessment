a10b9f767040297e95f72d3b3fda497878e21467
sam4rano <oyerindesamuelabiodun@gmail.com>
2025-01-20 16:36:55 +0100

    hosting issues

--- app/config/api.tsx
+++ app/config/api.tsx
@@ -1,16 +1,21 @@
+const BASE_PROXY =
+  process.env.NODE_ENV === "production"
+    ? "/api/proxy"
+    : process.env.NEXT_PUBLIC_API_BASE_URL;
+
 export const API_ENDPOINTS = {
-	BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-	USER: {
-	  BASE: `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_USER_MANAGER_PATH}`,
-	  LOGIN: '/login',
-	  CHECK_OTP: '/checkOTP',
-	},
-	CONFIG: {
-	  BRANCH: `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_CONFIG_PATH}/branch`,
-	  CURRENCY: `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_CONFIG_PATH}/currency/get_currency_by_number`,
-	},
-	ORDER_DELIVERY: {
-	  CASH_REQUEST: `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_ORDER_DELIVERY_PATH}/cash-request`,
-	  CASH_EVACUATION: `${process.env.NEXT_PUBLIC_API_BASE_URL}${process.env.NEXT_PUBLIC_ORDER_DELIVERY_PATH}/cash-evacuation`,
-	},
-  };
+  BASE_URL: BASE_PROXY,
+  USER: {
+    BASE: `${BASE_PROXY}/user-manager/user`,
+    LOGIN: "/login",
+    CHECK_OTP: "/checkOTP",
+  },
+  CONFIG: {
+    BRANCH: `${BASE_PROXY}/configuration/branch`,
+    CURRENCY: `${BASE_PROXY}/configuration/currency/get_currency_by_number`,
+  },
+  ORDER_DELIVERY: {
+    CASH_REQUEST: `${BASE_PROXY}/order-and-delivery/cash-request`,
+    CASH_EVACUATION: `${BASE_PROXY}/order-and-delivery/cash-evacuation`,
+  },
+};
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,25 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/user-manager/:path*',
+        destination: 'http://20.126.3.61:3400/user-manager/:path*'
+      },
+      {
+        source: '/api/proxy/configuration/:path*',
+        destination: 'http://20.126.3.61:3400/configuration/:path*'
+      },
+      {
+        source: '/api/proxy/order-and-delivery/:path*',
+        destination: 'http://20.126.3.61:3400/order-and-delivery/:path*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
+
+
